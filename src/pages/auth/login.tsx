98c70e9b0859684b48d0117d4ab19f78dd685775
Daniel <danielsimamorasimamora@gmail.com>
2025-10-08 12:38:25 +0700

    login di tambahin

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
     }
   return (
     <div>
-        <h1>Login Page</h1>
+        <h1>Login Page</h1> 
         <button onClick={handleLogin}>Login</button>
         belum punya akun? <Link href="/auth/register">Register</Link>
     </div>
